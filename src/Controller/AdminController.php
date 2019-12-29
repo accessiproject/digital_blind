10bc3198f2bf0a8a6bc90b2a40647dab2fc7668f
accessiproject <kevin.bustamante@mail.novancia.fr>
2019-12-29 22:29:06 +0100

    add index profil

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,13 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-    * @Route("/", name="home_default") 
-    * @Route("/accueil", name="home_index")
+    * @Route("/", name="admin_profil_index") 
      */
-    public function index()
+    public function profil_index()
     {
         $company = $this->getDoctrine()->getRepository(Company::class)->find(1);
-        return $this->render('home/index.html.twig', [
+        return $this->render('admin/profil/index.html.twig', [
             'controller_name' => 'AdminController',
             'company' => $company,
         ]);
