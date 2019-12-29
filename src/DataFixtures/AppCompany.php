e7e21ffc5c74900641b8477f099019f2a0acd0e3
accessiproject <kevin.bustamante@mail.novancia.fr>
2019-12-29 15:40:58 +0100

    creation Company faker

--- src/DataFixtures/AppCompany.php
+++ src/DataFixtures/AppCompany.php
@@ -5,19 +5,25 @@ namespace App\DataFixtures;
 use App\Entity\Company;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
 class AppCompany extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // initialisation de l'objet Faker
+        // on peut préciser en paramètre la localisation, 
+        // pour avoir des données qui semblent "françaises"
+        $faker = Faker\Factory::create('fr_FR');
+        
         $company = new Company();
         $company->setName("Digital Blind")
             ->setLogo("image\logo")
-            ->setSummary("Résumé")
-            ->setPresentation("Présentation")
-            ->setAddress("39 avenue Georges Bernanos")
-            ->setPostalcode("75005")
-            ->setCity("Paris")
+            ->setSummary("Voici le résumé !")
+            ->setPresentation("Voici la présentation")
+            ->setAddress($faker->address)
+            ->setPostalcode($faker->postcode)
+            ->setCity($faker->city)
             ->setPhone("0254822623")
             ->setMobile("0630300904");
         $manager->persist($company);
